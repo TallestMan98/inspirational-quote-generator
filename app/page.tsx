fe3d10e42a87ea04b8022658ef91fb5f1f7efa61
Sebastian Barrett <sbarrett@praestoconsulting.com>
2023-06-20 17:54:18 +0100

    Funky pipes!

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import { GradientBackgroundCon, Pipe1, Pipe2, Pipe3, Pipe4, PipeTop, PipeFlow, PipeBottom, HorizontalPipe1, HorizontalPipe2, HorizontalFlowRight, HorizontalFlowLeft, PipeMiddle } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 import Image from 'next/image'
 
 export default function Home() {
@@ -7,6 +7,54 @@ export default function Home() {
     <>
       {/* Background */}
       <GradientBackgroundCon>
+        <HorizontalPipe1>
+          <PipeMiddle>
+          </PipeMiddle>
+          <HorizontalFlowRight>
+          </HorizontalFlowRight>
+          <HorizontalFlowLeft>
+          </HorizontalFlowLeft>
+        </HorizontalPipe1>
+        <HorizontalPipe2>
+          <PipeMiddle>
+          </PipeMiddle>
+          <HorizontalFlowRight>
+          </HorizontalFlowRight>
+          <HorizontalFlowLeft>
+          </HorizontalFlowLeft>
+        </HorizontalPipe2>
+        <Pipe1>
+          <PipeTop>
+          </PipeTop>
+          <PipeFlow>
+          </PipeFlow>
+          <PipeBottom>
+          </PipeBottom>
+        </Pipe1>
+        <Pipe2>
+          <PipeTop>
+          </PipeTop>
+          <PipeFlow>
+          </PipeFlow>
+          <PipeBottom>
+          </PipeBottom>
+        </Pipe2>
+        <Pipe3>
+          <PipeTop>
+          </PipeTop>
+          <PipeFlow>
+          </PipeFlow>
+          <PipeBottom>
+          </PipeBottom>
+        </Pipe3>
+        <Pipe4>
+          <PipeTop>
+          </PipeTop>
+          <PipeFlow>
+          </PipeFlow>
+          <PipeBottom>
+          </PipeBottom>
+        </Pipe4>
       </GradientBackgroundCon>
     </>
   )
--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -1,22 +1,157 @@
 import styled from "styled-components";
-import Link from "next/link";
-import Image from "next/image";
+//import Link from "next/link";
+//import Image from "next/image";
 
 export const GradientBackgroundCon = styled.div`
   background: linear-gradient(to bottom,#e66465, #9198e5);
   background-size: 300% 300%;
   height: 100vh;
   width: 100vw;
-  animation: gradient 10s ease infinite;
+  animation: gradient 20s ease infinite;
   @keyframes gradient {
     0% {
-        background-position: 50% 0%;
+        background-position: 50% 10%;
     }
     50% {
-        background-position: 50% 100%;
+        background-position: 50% 80%;
     }
     100% {
-        background-position: 50% 0%;
+        background-position: 50% 10%;
+    }
+  }
+`;
+
+export const Pipe1 = styled.div`
+  background-color: #000050;
+  width: 0.25vw;
+  height: 80vh;
+  position: fixed;
+  right: 90%;
+  top: 15%;
+`;
+
+export const Pipe2 = styled.div`
+  background-color: #000050;
+  width: 0.25vw;
+  height: 60vh;
+  position: fixed;
+  right: 85%;
+  top: 25%;
+`;
+
+export const Pipe3 = styled.div`
+  background-color: #000050;
+  width: 0.25vw;
+  height: 80vh;
+  position: fixed;
+  right: 10%;
+  top: 15%;
+`;
+
+export const Pipe4 = styled.div`
+  background-color: #000050;
+  width: 0.25vw;
+  height: 60vh;
+  position: fixed;
+  right: 15%;
+  top: 25%;
+`;
+
+export const HorizontalPipe1 = styled.div`
+  background-color: #000050;
+  width: 81.3vw;
+  height: 0.25vw;
+  position: absolute;
+  right: 9.5vw;
+  top: 94vh;
+  
+`;
+export const HorizontalPipe2 = styled.div`
+  background-color: #000050;
+  width: 71vw;
+  height: 0.25vw;
+  position: absolute;
+  right: 14.7vw;
+  top: 84vh;
+`;
+
+export const PipeTop = styled.div`
+  background-color: white;
+  border-radius: 20%;
+  width: 2vw;
+  height: 2vw;
+  position: absolute;
+  left: -0.9vw;
+  z-index: 10;
+`;
+
+export const PipeBottom = styled.div`
+  background-color: white;
+  border-radius: 20%;
+  width: 2vw;
+  height: 2vw;
+  position: absolute;
+  left: -0.9vw;
+  z-index: 10;
+  bottom: 0;
+`;
+
+export const PipeMiddle = styled.div`
+  background-color: white;
+  border-radius: 20%;
+  width: 2vw;
+  height: 2vw;
+  position: absolute;
+  z-index: 10;
+  right: 48.7%;
+  bottom: -300%;
+
+`;
+
+export const PipeFlow = styled.div`
+  background: linear-gradient(to bottom, black, white, white, white, black);
+  width: 100%;
+  height: 10%;
+  position: relative;
+  animation: flow 10s ease-in-out infinite alternate;
+  @keyframes flow {
+    0% {
+      top: 0%;
+    }
+    100% {
+      top: 90%;
+    }
+  }
+`;
+
+export const HorizontalFlowRight = styled.div`
+  background: linear-gradient(to right, black, white, white, white, black);
+  width: 7%;
+  height: 100%;
+  position: absolute;
+  animation: flowRight 10s ease-in-out infinite alternate;
+  @keyframes flowRight {
+    0% {
+      left: 44%;
+    }
+    100% {
+      left: 0%;
+    }
+  }
+`;
+export const HorizontalFlowLeft = styled.div`
+  background: linear-gradient(to left, black, white, white, white, black);
+  width: 7%;
+  height: 100%;
+  position: absolute;
+  animation: flowLeft 10s ease-in-out infinite alternate;
+  @keyframes flowLeft {
+    0% {
+      right: 44%;
+    }
+
+    100% {
+      right: 0%;
     }
   }
 `;
