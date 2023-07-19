4f0e4c53fe6a6f7a7fe438a204d9a2b4b197b9dd
Sebastian Barrett <sbarrett@praestoconsulting.com>
2023-07-19 17:10:29 +0100

    CSS for webpage background at a good point. Footer Added. Ready for some foreground functionality in front end :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,43 @@
 'use client'
-import { GradientBackgroundCon, Pipe1, Pipe2, Pipe3, Pipe4, PipeTop, PipeFlow, PipeBottom, HorizontalPipe1, HorizontalPipe2, HorizontalFlowRight, HorizontalFlowLeft, PipeMiddle } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import { GradientBackgroundCon, Pipe1, Pipe2, Pipe3, Pipe4, PipeFlow, FooterContainer, FooterLink } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       {/* Background */}
       <GradientBackgroundCon>
-        <HorizontalPipe1>
-          <PipeMiddle>
-          </PipeMiddle>
-          <HorizontalFlowRight>
-          </HorizontalFlowRight>
-          <HorizontalFlowLeft>
-          </HorizontalFlowLeft>
-        </HorizontalPipe1>
-        <HorizontalPipe2>
-          <PipeMiddle>
-          </PipeMiddle>
-          <HorizontalFlowRight>
-          </HorizontalFlowRight>
-          <HorizontalFlowLeft>
-          </HorizontalFlowLeft>
-        </HorizontalPipe2>
         <Pipe1>
-          <PipeTop>
-          </PipeTop>
-          <PipeFlow>
-          </PipeFlow>
-          <PipeBottom>
-          </PipeBottom>
+          <PipeFlow></PipeFlow>
         </Pipe1>
         <Pipe2>
-          <PipeTop>
-          </PipeTop>
-          <PipeFlow>
-          </PipeFlow>
-          <PipeBottom>
-          </PipeBottom>
+          <PipeFlow></PipeFlow>
         </Pipe2>
         <Pipe3>
-          <PipeTop>
-          </PipeTop>
-          <PipeFlow>
-          </PipeFlow>
-          <PipeBottom>
-          </PipeBottom>
+          <PipeFlow></PipeFlow>
         </Pipe3>
         <Pipe4>
-          <PipeTop>
-          </PipeTop>
-          <PipeFlow>
-          </PipeFlow>
-          <PipeBottom>
-          </PipeBottom>
+          <PipeFlow></PipeFlow>
         </Pipe4>
       </GradientBackgroundCon>
+
+      {/* Quote Generator Modal*/}
+      {/* <QuoteGeneratorModal
+      /> */}
+
+      
+
+      {/* Footer */}
+      <FooterContainer>
+        <>
+          Quotes Generated: {numberOfQuotes}
+          <br/>
+          Developed by <FooterLink href="https://github.com/TallestMan98" target="_blank"> @SebBarrett</FooterLink>
+        </>
+      </FooterContainer>
     </>
   )
 }
--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-//import Link from "next/link";
+import Link from "next/link";
 //import Image from "next/image";
 
 export const GradientBackgroundCon = styled.div`
@@ -23,97 +23,64 @@ export const GradientBackgroundCon = styled.div`
 
 export const Pipe1 = styled.div`
   background-color: #000050;
-  width: 0.25vw;
+  width: 2px;
   height: 80vh;
   position: fixed;
-  right: 90%;
-  top: 15%;
+  right: 95vw;
+  top: 10vh;
 `;
 
 export const Pipe2 = styled.div`
   background-color: #000050;
-  width: 0.25vw;
+  width: 2px;
   height: 60vh;
   position: fixed;
-  right: 85%;
-  top: 25%;
+  right: 90vw;
+  top: 20vh;
 `;
 
 export const Pipe3 = styled.div`
   background-color: #000050;
-  width: 0.25vw;
-  height: 80vh;
+  width: 2px;
+  height: 60vh;
   position: fixed;
-  right: 10%;
-  top: 15%;
+  right: 10vw;
+  top: 20vh;
 `;
 
 export const Pipe4 = styled.div`
   background-color: #000050;
-  width: 0.25vw;
-  height: 60vh;
+  width: 2px;
+  height: 80vh;
   position: fixed;
-  right: 15%;
-  top: 25%;
-`;
-
-export const HorizontalPipe1 = styled.div`
-  background-color: #000050;
-  width: 81.3vw;
-  height: 0.25vw;
-  position: absolute;
-  right: 9.5vw;
-  top: 94vh;
-  
-`;
-export const HorizontalPipe2 = styled.div`
-  background-color: #000050;
-  width: 71vw;
-  height: 0.25vw;
-  position: absolute;
-  right: 14.7vw;
-  top: 84vh;
-`;
-
-export const PipeTop = styled.div`
-  background-color: white;
-  border-radius: 20%;
-  width: 2vw;
-  height: 2vw;
-  position: absolute;
-  left: -0.9vw;
-  z-index: 10;
-`;
-
-export const PipeBottom = styled.div`
-  background-color: white;
-  border-radius: 20%;
-  width: 2vw;
-  height: 2vw;
-  position: absolute;
-  left: -0.9vw;
-  z-index: 10;
-  bottom: 0;
-`;
-
-export const PipeMiddle = styled.div`
-  background-color: white;
-  border-radius: 20%;
-  width: 2vw;
-  height: 2vw;
-  position: absolute;
-  z-index: 10;
-  right: 48.7%;
-  bottom: -300%;
-
+  right: 5vw;
+  top: 10vh;
 `;
 
 export const PipeFlow = styled.div`
-  background: linear-gradient(to bottom, black, white, white, white, black);
+  background: linear-gradient(to bottom, #000050, white, white, white, #000050);
   width: 100%;
   height: 10%;
+  background-size: 130% 130%;
   position: relative;
-  animation: flow 10s ease-in-out infinite alternate;
+  animation: flow 10s ease-in-out infinite alternate, colourChange 10s ease-in-out infinite alternate;
+  @keyframes colourChange {
+    0% {
+        background-position: 50% 100%;
+    }
+    20% {
+      background-position: 50% 50%;
+      background-size: 100% 100%;
+    }
+    80% {
+      background-position: 50% 50%;
+      background-size: 100% 100%;
+    }
+    100% {
+        background-position: 50% 0%;
+    }
+  }
+
   @keyframes flow {
     0% {
       top: 0%;
@@ -124,34 +91,18 @@ export const PipeFlow = styled.div`
   }
 `;
 
-export const HorizontalFlowRight = styled.div`
-  background: linear-gradient(to right, black, white, white, white, black);
-  width: 7%;
-  height: 100%;
+export const FooterContainer = styled.div`
+  width: 100vw;
+  height: 15%;
+  text-align: center;
+  font-family: 'Source Code Pro';
+  font-size: 20px;
   position: absolute;
-  animation: flowRight 10s ease-in-out infinite alternate;
-  @keyframes flowRight {
-    0% {
-      left: 44%;
-    }
-    100% {
-      left: 0%;
-    }
-  }
+  bottom: 0;
+  color: white;
 `;
-export const HorizontalFlowLeft = styled.div`
-  background: linear-gradient(to left, black, white, white, white, black);
-  width: 7%;
-  height: 100%;
-  position: absolute;
-  animation: flowLeft 10s ease-in-out infinite alternate;
-  @keyframes flowLeft {
-    0% {
-      right: 44%;
-    }
 
-    100% {
-      right: 0%;
-    }
-  }
+export const FooterLink = styled(Link)`
+  color: white;
+  font-family: 'Comfortaa';
 `;
